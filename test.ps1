483910cc8ca4676f0c66efacf58d1690ab33f3a2
Philip Peitsch <philip.peitsch@gmail.com>
2022-08-17 14:06:04 +1000

    Helper test script

---
+++ test.ps1
@@ -0,0 +1,14 @@
+Push-Location tsc43
+Write-Host tsc43
+npx tsc
+Pop-Location
+
+Push-Location tsc44
+Write-Host tsc44
+npx tsc
+Pop-Location
+
+Push-Location tsc47
+Write-Host tsc47
+npx tsc
+Pop-Location
