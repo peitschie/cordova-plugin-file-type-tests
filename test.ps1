a5c7cbca670291c5c03bf18cdea0e48f5e285bbd
Philip Peitsch <philip.peitsch@gmail.com>
2023-07-28 12:18:21 +1000

    Automatically copy type from root to all combinations

--- test.ps1
+++ test.ps1
@@ -1,14 +1,20 @@
 Push-Location tsc43
 Write-Host tsc43
+npm install
+Copy-Item ..\cordova-plugin-file.d.ts node_modules\cordova-plugin-file\types\index.d.ts
 npx tsc
 Pop-Location
 
 Push-Location tsc44
 Write-Host tsc44
+npm install
+Copy-Item ..\cordova-plugin-file.d.ts node_modules\cordova-plugin-file\types\index.d.ts
 npx tsc
 Pop-Location
 
 Push-Location tsc47
 Write-Host tsc47
+npm install
+Copy-Item ..\cordova-plugin-file.d.ts node_modules\cordova-plugin-file\types\index.d.ts
 npx tsc
 Pop-Location
